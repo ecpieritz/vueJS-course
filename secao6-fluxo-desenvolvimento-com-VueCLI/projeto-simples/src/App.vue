72f8b99805dcea715d8b95aedf074a8a604fd305
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-03-24 20:13:00 -0300

    Added simple project
    Used to understand the structure that's created with VueCLI

---
+++ secao6-fluxo-desenvolvimento-com-VueCLI/projeto-simples/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <button @click="title += ' :D'">Change</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+      return{title: 'Testando...'
+    }
+  }
+}
+</script>
+
+<style>
+body{
+  background-color: black;
+  color: white;
+}
+#app {
+  margin-top: 60px; 
+}
+</style>
