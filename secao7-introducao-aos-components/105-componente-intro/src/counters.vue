aa172ac07877a368526d951652e21423055b3822
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-03-27 15:00:55 -0300

    Added lesson 105

---
+++ secao7-introducao-aos-components/105-componente-intro/src/counters.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="counters">
+        <h1>COUNTERS</h1>
+        <app-counter v-for="c in 5" :key="c" />
+    </div>
+</template>
+
+<script>
+import Counter from './counter.vue' //registrado localmente
+
+export default {
+    components:{
+        'app-counter': Counter
+    }
+}
+</script>
+
+<style>
+    
+</style>
