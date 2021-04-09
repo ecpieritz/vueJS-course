297e4b056631871d882cbec778c2d8ab79e7b861
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-04-09 11:10:27 -0300

    added lessons 125 & 126

---
+++ secao8-comunicacao-entre-componentes/125-126-usando-event-bus-para-comunicacao-entre-componentes-irmaos/src/components/UsuarioEditar.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="componente">
+        <h2>Alterar os Dados de Usuário</h2>
+        <p>Edite as informações</p>
+        <p>Idade do usuário: <strong>{{idade}}</strong></p>
+        <button @click="changeAge">Alterar Idade</button>
+    </div>
+</template>
+
+<script>
+import barramento from '@/barramento'
+
+export default {
+    props: {
+        idade: Number
+    },
+    methods: {
+        changeAge(){
+            this.idade = 33
+            barramento.$emit('changeAge', 33)
+        }        
+    },
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #98b99a;
+        color: #fff;
+    }
+     button{
+        font-family: 'Rubik', sans-serif;
+        padding: 10px 20px;
+        background-color: #518a55;
+        color: #ffffff;
+        border: none;
+        border-radius: 20px;
+        cursor: pointer;
+    }
+
+    button:hover{
+        background-color: #325e35;
+        color: #afaeae;
+
+    }
+</style>
