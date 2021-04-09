ea0117af680ea300cc38b4d94ab8e37b2e388ee5
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-04-09 13:07:32 -0300

    added lessons 128 & 129

---
+++ secao8-comunicacao-entre-componentes/128-129-pratica-comunicacao-entre-componentes/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+	<div id="app">
+		<usuario-lista :usuarios="usuarios" />
+		<usuario-detalhe />
+	</div>
+</template>
+
+<script>
+import UsuarioLista from './components/UsuarioLista.vue'
+import UsuarioDetalhe from './components/UsuarioDetalhe.vue'
+
+export default {
+	name: "app",
+	components: { UsuarioLista, UsuarioDetalhe },
+    data() {
+        return {
+            usuarios: [
+                { id: 1, nome: 'Deva', idade: 31 },
+                { id: 2, nome: 'Emilyn', idade: 29 },
+                { id: 3, nome: 'Alfredo', idade: 56 },
+                { id: 4, nome: 'Inês', idade: 27 },
+                { id: 5, nome: 'Bruna', idade: 24 }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+
+	display: flex;
+	font-size: 2rem;
+}
+
+#app div {
+	margin: 20px;
+}
+</style>
