0735cd0f3540a4c879c2b4a8a6c90254a4b96704
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-04-12 17:54:22 -0300

    Added lessons 135, 136 & 137

---
+++ secao9-uso-avancado-de-componentes/135-136-137-slots/src/components/Citacao.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="citacao">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        border-radius: 8px;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        margin-top: 2vw;
+        max-width: 450px;
+    }
+</style>
