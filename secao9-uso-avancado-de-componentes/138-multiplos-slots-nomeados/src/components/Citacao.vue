37879462b62dd9694cb7b02c1ec89547558e2f44
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-04-12 18:11:57 -0300

    Added lesson 138

---
+++ secao9-uso-avancado-de-componentes/138-multiplos-slots-nomeados/src/components/Citacao.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="citacao">
+        <slot name="texto"></slot>
+        <slot name="fonte"></slot>
+        <slot name="autor"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        border-radius: 8px;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        margin-top: 2vw;
+        max-width: 450px;
+    }
+</style>
---
+++ secao9-uso-avancado-de-componentes/138-multiplos-slots-nomeados/src/components/Citacoes.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="citacoes">
+        <span>
+            <button @click="numero--">&lt;</button>
+            <button @click="numero++">&gt;</button>
+        </span>
+        <Citacao>
+            <h3 slot="texto">{{citacoes[indice].texto}}</h3>
+            <h4 slot="fonte"><i>{{citacoes[indice].fonte}}</i></h4>
+            <h6 slot="autor">{{citacoes[indice].autor}}</h6>
+        </Citacao >
+        <!-- <h3>{{citacoes[indice].texto}}</h3> -->
+    </div>
+</template>
+
+<script>
+import Citacao from './Citacao.vue'
+
+export default {
+    components: {Citacao},
+    data() {
+        return {
+            numero: 0,
+            citacoes: [{
+                fonte: 'Conecta Elas',
+                texto: 'Quando eu concordo que li todos os termos de condições antes de colocar o programa pra instalar...',
+                autor: '@conecta_elas'
+            }, { 
+                fonte: 'Dev Sincero',
+                texto: 'O primeiro passo é decidir se você pretende atuar como desenvolvedor front-end, back-end ou até mesmo full-stack.',
+                autor: '@dev_sincero'
+            }, {
+                fonte: 'Elas Programam',
+                texto: 'Sorte é estar preparada quando a oportunidade aparece.',
+                autor: '@elasprogramam'
+            }]
+        }
+    },
+    computed: {
+        indice() {
+            return Math.abs(this.numero % 3)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .citacoes {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+    }
+
+    .citacoes h3{
+        color: #801240;
+    }
+    .citacoes h4{
+        text-align: right;
+        margin-top: -20px;
+    }
+    .citacoes h6{
+        text-align: right;
+        margin-top: -30px;
+    }
+
+</style>
