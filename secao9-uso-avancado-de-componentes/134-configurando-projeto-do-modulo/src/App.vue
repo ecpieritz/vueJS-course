ee6c8159d672632a93783dae4d7d9ab35c03e027
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-04-12 17:34:11 -0300

    Added lesson 134

---
+++ secao9-uso-avancado-de-componentes/134-configurando-projeto-do-modulo/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+	<div id="app">
+		<Citacoes />
+		<Sobre />
+	</div>
+</template>
+
+<script>
+import Citacoes from './components/Citacoes'
+import Sobre from './components/Sobre'
+
+export default {
+	components: { Citacoes, Sobre }
+}
+</script>
+
+<style>
+	body{
+		margin: 0;
+		padding: 0;
+	}
+	#app {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		font-size: 1.6rem;
+	}
+
+	button {
+        outline: none;
+        padding: 5px 15px 5px;
+        margin: 10px 5px;
+        border-radius: 8px;
+        font-size: 2rem;
+        font-weight: 300;
+        color: #FFF;
+        background-color: #55042a80;
+		cursor: pointer;
+    }
+</style>
---
+++ secao9-uso-avancado-de-componentes/134-configurando-projeto-do-modulo/src/components/Citacoes.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="citacoes">
+        <span>
+            <button @click="numero--">&lt;</button>
+            <button @click="numero++">&gt;</button>
+        </span>
+        {{ numero }} {{ indice }}
+        <h3>{{citacoes[indice].texto}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            numero: 0,
+            citacoes: [{
+                fonte: 'Conecta Elas',
+                texto: 'Quando eu concordo que li todos os termos de condições antes de colocar o programa pra instalar...',
+                autor: '@conecta_elas'
+            }, { 
+                fonte: 'Dev Sincero',
+                texto: 'O primeiro passo é decidir se você pretende atuar como desenvolvedor front-end, back-end ou até mesmo full-stack.',
+                autor: '@dev_sincero'
+            }, {
+                fonte: 'Elas Programam',
+                texto: 'Sorte é estar preparada quando a oportunidade aparece.',
+                autor: '@elasprogramam'
+            }]
+        }
+    },
+    computed: {
+        indice() {
+            return Math.abs(this.numero % 3)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .citacoes {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+    }
+</style>
