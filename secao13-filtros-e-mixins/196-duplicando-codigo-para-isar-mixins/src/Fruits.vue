7092cd5d5051e50039bdc309d4e9327889045b10
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-05 14:36:13 -0300

    Added lesson 196

---
+++ secao13-filtros-e-mixins/196-duplicando-codigo-para-isar-mixins/src/Fruits.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="fruit in fruits" :key="fruit">{{fruit}}</li>
+        </ul>
+        <input type="text" v-model="fruit" @keydown.enter="add">
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            fruit: '',
+            fruits: ['banana', 'apple', 'orange']
+        }
+    },
+    methods: {
+        add(){
+            this.fruits.push(this.fruit)
+            this,fruit = ''
+        }
+    },
+}
+</script>
+
+<style>
+    
+</style>
