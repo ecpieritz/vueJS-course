81b130efa0d82502038386d7752f7a2a6994d36c
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-09 17:27:06 -0300

    Added lessons 206,207 & 208 - part 1

---
+++ secao14-animacoes-e-transicoes/206-207-208-criando-transicao-fade/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+	<div id="app" class="container-fluid">
+		<h1>Animações</h1>
+		<hr>
+		<b-button variant='primary' class="mb-4" @click="display = !display">Show Message</b-button>
+		
+		<transition name="fade">
+			<b-alert variant='info' show v-if="display">{{msg}}</b-alert>
+		</transition>
+	</div>
+</template>
+
+<script>
+
+export default {
+	data(){
+		return {
+			msg: 'Uma mensagem de informação para o usuário!',
+			display: false
+		}
+	}
+}
+</script>
+
+<style>
+body{
+	background-color: #2e2e2e;
+}
+h1{
+	color: #0083fd;
+	font-weight: 900;
+}
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	margin-top: 60px;
+	color: #fff;
+	font-size: 1.5rem;
+}
+
+.fade-enter{
+
+}
+
+.fade-enter-active{
+
+}
+
+.fade-enter-to{
+
+}
+
+.fade-leave{
+
+}
+
+.fade-leave-active{
+
+}
+
+.fade-leave-to{
+	
+}
+</style>
