442d705a96769948befea9aa9c0bbabdfbf6a9cc
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-11 13:18:23 -0300

    Added lesson 220

---
+++ 220-animando-componentes-dinamicos/src/InfoAlert.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <b-alert variant='info' show>An important message!</b-alert>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ 220-animando-componentes-dinamicos/src/WarningAlert.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <b-alert variant='warning' show>An important message!</b-alert>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
