c9628e7290c99e7de16b1d73556a92c49f65dcd8
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-11 10:18:42 -0300

    Added lessons 216 & 217

---
+++ secao14-animacoes-e-transicoes/216-217-entendendo-animacao-com-javascript/src/App.vue
@@ -0,0 +1,151 @@
+<template>
+	<div id="app" class="container-fluid">
+		<h1>Animações</h1>
+		<hr>
+		<b-button variant='primary' class="mb-4" @click="display = !display">Show Message</b-button>
+		<!-- 
+		<transition name="fade">
+			<b-alert variant='info' show v-if="display">{{msg}}</b-alert>
+		</transition>
+
+		<transition name="slide">
+			<b-alert variant='info' show v-show="display">{{msg}}</b-alert>
+		</transition>
+
+		<transition 
+		enter-active-class="animated bounce" 
+		leave-active-class="animated shake">
+			<b-alert variant='info' show v-show="display">{{msg}}</b-alert>
+		</transition> -->
+
+		<hr>
+
+		<b-select v-model="animationType" class="mb-4">
+			<option value="fade">Fade</option>
+			<option value="slide">Slide</option>
+		</b-select>
+
+		<transition :name="animationType" mode="out-in">
+			<b-alert variant='info' show v-if="display" key="info">{{msg}}</b-alert>
+			<b-alert variant='warning' show v-else key="warn">{{msg}}</b-alert>
+		</transition>
+
+		<hr>
+		<button @click="display2 = !display2">Show</button>
+		<transition
+			@before-enter="beforeEnter"
+			@enter="enter"
+			@after-enter="afterEnter"
+			@enter-cancelled="enterCancelled"
+			@before-leave="beforeLeave"
+			@leave="leave"
+			@after-leave="afterLeave"
+			@leave-cancelled="leaveCancelled">
+			<div v-if="display2" class="box"></div>
+		</transition>
+	</div>
+</template>
+
+<script>
+
+export default {
+	data(){
+		return {
+			msg: 'Uma mensagem de informação para o usuário!',
+			display: false,
+			display2: true,
+			animationType: 'fade'
+		}
+	},
+	methods: {
+		beforeEnter(el){
+			console.log('beforeEnter')
+		},
+		enter(el, done){
+			console.log('enter')
+			done()
+		},
+		afterEnter(el){
+			console.log('afterEnter')
+		},
+		enterCancelled(el){
+			console.log('enterCancelled')
+		},
+		beforeLeave(el){
+			console.log('beforeLeave')
+		},
+		leave(el, done){
+			console.log('leave')
+			done()
+		},
+		afterLeave(el){
+			console.log('afterLeave')
+		},
+		leaveCancelled(el){
+			console.log('leaveCancelled')
+		},
+	},
+}
+</script>
+
+<style>
+body{
+	background-color: #2e2e2e;
+}
+h1{
+	color: #0083fd;
+	font-weight: 900;
+}
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	margin-top: 60px;
+	color: #fff;
+	font-size: 1.5rem;
+}
+
+.box{
+	height: 100px;
+	width: 300px;
+	margin: 30px auto;
+	background-color: #859de0;
+}
+
+.fade-enter,
+.fade-leave-to{
+	opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active{
+	transition: opacity 2s;
+}
+
+@keyframes slide-in {
+	from { transform: translateY(40px);}
+	to {transform: translateY(0);}
+}
+
+@keyframes slide-out {
+	from { transform: translateY(0);}
+	to {transform: translateY(40px);}
+}
+
+.slide-enter-active{
+	animation: slide-in 2s ease;
+	transition: opacity 2s;
+}
+
+.slide-leave-active{
+	animation: slide-out 2s ease;
+	transition: opacity 2s;
+}
+
+.slide-enter,
+.slide-leave-to{
+	opacity: 0;
+}
+
+</style>
