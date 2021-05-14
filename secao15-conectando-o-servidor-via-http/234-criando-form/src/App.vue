42e1ad3995a0a18625c4f2d69270116a653691fc
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-14 09:29:51 -0300

    Added lesson 234

---
+++ secao15-conectando-o-servidor-via-http/234-criando-form/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+	<div id="app" class="container">
+		<h1>HTTP com Axios</h1>
+		<b-card>
+			<b-form-group label="Name:">
+				<b-form-input type="text" size="lg" v-model="user.name" placeholder="Enter the name..."></b-form-input>
+			</b-form-group>
+			<b-form-group label="Email:">
+				<b-form-input type="text" size="lg" v-model="user.email" placeholder="Enter the email..."></b-form-input>
+			</b-form-group>
+			<hr>
+			<b-button @click="save" size="lg" variant="primary">Save</b-button>
+		</b-card>
+	</div>
+</template>
+
+<script>
+export default {
+	data(){
+		return{
+			user: {
+				name: '',
+				email: ''
+			}
+		}
+	},
+	methods: {
+		save(){
+			
+		}
+	},
+//   created() {
+// 		this.$http.post('users.json', {
+// 			name: 'Elsa',
+// 			email: 'let_it_go_elsa@gmail.com'
+// 		},
+// 		).then(res => console.log(res))       
+//   },
+}
+</script>
+
+<style>
+body{
+	background: linear-gradient(to right, #19276b 0%, #70136a 100%);
+  }
+#app {
+	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #0e1849;
+	font-size: 1.5rem;
+}
+
+#app h1 {
+	text-align: center;
+	margin: 50px;
+  color: #FFCC70;
+;
+}
+</style>
