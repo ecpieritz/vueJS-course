50745861424d816b7c79650c812b3d04449b55f4
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-14 09:09:21 -0300

    Added lesson 233

---
+++ secao15-conectando-o-servidor-via-http/233-criando-instancia-axios/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+	<div id="app" class="container">
+		<h1>HTTP com Axios</h1>
+	</div>
+</template>
+
+<script>
+export default {
+  created() {
+		this.$http.post('users.json', {
+			name: 'Elsa',
+			email: 'let_it_go_elsa@gmail.com'
+		},
+		).then(res => console.log(res))       
+  },
+}
+</script>
+
+<style>
+body{
+	background: linear-gradient(to right, #19276b 0%, #70136a 100%);
+  }
+#app {
+	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #ffffff;
+	font-size: 1.5rem;
+}
+
+#app h1 {
+	text-align: center;
+	margin: 50px;
+  color: #FFCC70;
+;
+}
+</style>
