869de0132cbcf635b6114f516f3a5f6c0a7a2d27
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-14 11:45:22 -0300

    Added lesson 238

---
+++ secao15-conectando-o-servidor-via-http/238-interceptando-requisicoes/src/plugins/axios.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+
+//axios.defaults.baseURL = 'https://curso-vue-e9fc3-default-rtdb.firebaseio.com/'
+
+Vue.use({
+    install(Vue){
+        //Vue.prototype.$http = axios
+        Vue.prototype.$http = axios.create({
+            baseURL: 'https://curso-vue-e9fc3-default-rtdb.firebaseio.com/'
+        })
+        Vue.prototype.$http.interceptors.request.use(config =>{
+          console.log(config.method)
+          return config
+        })
+    }
+})
+
