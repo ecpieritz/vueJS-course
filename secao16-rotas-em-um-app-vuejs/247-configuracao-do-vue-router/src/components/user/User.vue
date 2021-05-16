4fce3fccbd968c5a2b3b9beae7623b75867ddb51
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-16 13:31:19 -0300

    Added lesson 247

---
+++ secao16-rotas-em-um-app-vuejs/247-configuracao-do-vue-router/src/components/user/User.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div class="user">
+    <h2>Users</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="">
+  
+</style>
---
+++ secao16-rotas-em-um-app-vuejs/247-configuracao-do-vue-router/src/router.js
@@ -0,0 +1,16 @@
+import Vue from "vue"
+import Router from "vue-router"
+import Start from "./components/Start"
+import User from "./components/user/User"
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+    path: '/',
+    component: Start
+  }, {
+    path: '/user',
+    component: User
+  }]
+})
