623282d6c3ea9f9744728796f3799c238cb97fb1
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-17 18:37:53 -0300

    Added lesson 249

---
+++ secao16-rotas-em-um-app-vuejs/249-navegando-com-router-links/src/components/template/Menu.vue
@@ -0,0 +1,16 @@
+<template lang="">
+  <nav class="menu">
+    <router-link to="/">Home</router-link>
+    <router-link to="/user">User</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="">
+  
+</style>
