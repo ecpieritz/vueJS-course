078d1db1db91fa8a572775b43d0de13d3090ab16
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-18 13:03:03 -0300

    Added lesson 254

---
+++ secao16-rotas-em-um-app-vuejs/254-mudancas-entre-parametros-de-rota-iguais/src/components/template/Menu.vue
@@ -0,0 +1,19 @@
+<template lang="">
+  <nav class="menu">
+    <ul>
+      <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+      <router-link to="/user/1" tag="li" active-class="active"><a>User 1</a></router-link>
+      <router-link to="/user/2" tag="li" active-class="active"><a>User 2</a></router-link>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="">
+  
+</style>
