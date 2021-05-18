e88c8582b69207daa7a79febd63e4a351fa4238a
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-18 13:17:33 -0300

    Added lesson 255

---
+++ secao16-rotas-em-um-app-vuejs/255-parametros-de-rota-via-props/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div class="start">
+    <h2>Start</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="">
+  
+</style>
---
+++ secao16-rotas-em-um-app-vuejs/255-parametros-de-rota-via-props/src/router.js
@@ -0,0 +1,17 @@
+import Vue from "vue"
+import Router from "vue-router"
+import Home from "./components/Home"
+import User from "./components/user/User"
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+    path: '/',
+    component: Home
+  }, {
+    path: '/user/:id',
+    component: User,
+    props: true
+  }]
+})
