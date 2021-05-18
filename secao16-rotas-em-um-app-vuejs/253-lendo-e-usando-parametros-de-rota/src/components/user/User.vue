27b00615c3bda6f697a2e062243b85b44cd08976
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-18 12:32:11 -0300

    Added lesson 253

---
+++ secao16-rotas-em-um-app-vuejs/253-lendo-e-usando-parametros-de-rota/src/components/user/User.vue
@@ -0,0 +1,27 @@
+<template lang="">
+  <div class="user">
+    <h2>Users</h2>
+    <hr>
+    <p><strong>Code: </strong> {{id}}</p>
+    
+    <button success @click="goToHome">Back</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{id: 
+    this.$route.params.id
+  }},
+  methods: {
+    goToHome(){
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style lang="">
+  
+</style>
