242de3cebb38917204417da0848f0d3194e73487
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-19 12:38:08 -0300

    Added lesson 269

---
+++ secao16-rotas-em-um-app-vuejs/269-beforeLeave/src/components/user/UserEdit.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="user-edit">
+    <h3>User Edit</h3>
+    <p><strong>Code: </strong> {{id}} / {{$route.params.id}}</p>
+    <p><strong>Complete: </strong>{{$route.query.complete ? 'Yes': 'No'}}</p>
+    <p><strong>Language: </strong>{{$route.query.language}}</p>
+    <hr>
+    <button primary @click="confirmed = true">Confirm</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  data(){
+    return{
+      confirmed: false
+    }
+  },
+  beforeRouteLeave(to, from, next){
+    if(this.confirmed){
+      next()
+    } else{
+      if(confirm('Are You Sure?')){
+        next()
+      }else{
+        next(false)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
