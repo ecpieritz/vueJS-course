f35f1fa833172abb4ad27a39eef40a58dbec6765
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-19 12:25:45 -0300

    Added lessons 267 & 268

---
+++ secao16-rotas-em-um-app-vuejs/267-268-beforeEnter/src/router.js
@@ -0,0 +1,52 @@
+import Vue from "vue"
+import Router from "vue-router"
+import Home from "./components/Home"
+import Menu from "./components/template/Menu"
+
+import User from "./components/user/User"
+import UserList from "./components/user/UserList"
+import UserEdit from "./components/user/UserEdit"
+import UserDetail from "./components/user/UserDetail"
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [{
+    name: 'home',
+    path: '/',
+    //component: Home
+    components: {
+      default: Home,
+      menu: Menu
+    }
+  }, {
+    path: '/user',
+    //component: User,
+    components:{
+      default: User,
+      menu: Menu,
+      menuBtm: Menu
+    },
+    props: true,
+    children: [
+      {path: '', component: UserList},
+      {path: ':id', component: UserDetail, props: true, beforeEnter: (to, from, next)=>{
+        console.log('antes da rota -> usuario detalhe')
+        next()
+      }},
+      {path: ':id/edit', component: UserEdit, props: true, name: 'editUser'}
+    ]
+  },{
+    path: '*',
+    redirect: '/'
+  }
+]
+})
+
+router.beforeEach((to, from, next) => {
+  console.log('antes das rotas -> global')
+  next()
+  
+})
+
+export default router
