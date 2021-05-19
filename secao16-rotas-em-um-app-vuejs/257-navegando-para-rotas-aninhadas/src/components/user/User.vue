0eaad3c989eb1700eab1544bc0cc5dd6770d06ef
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-19 09:17:54 -0300

    Added lesson 257

---
+++ secao16-rotas-em-um-app-vuejs/257-navegando-para-rotas-aninhadas/src/components/user/User.vue
@@ -0,0 +1,22 @@
+<template lang="">
+  <div class="user">
+    <h2>Users</h2>
+    <hr>
+    <router-view/>
+    <button success @click="goToHome">Back</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToHome(){
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style lang="">
+  
+</style>
---
+++ secao16-rotas-em-um-app-vuejs/257-navegando-para-rotas-aninhadas/src/components/user/UserList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="user-list">
+    <h3>User List</h3>
+    <ul class="list">
+      <router-link tag='li' to='/user/1' class="list-item">User 1</router-link>
+      <router-link tag='li' to='/user/2' class="list-item">User 2</router-link>
+      <router-link tag='li' to='/user/3' class="list-item">User 3</router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
