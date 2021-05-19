ea6a9be790b8502f84c2cd39682e1497c275dcca
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-19 09:24:56 -0300

    Added lesson 258

---
+++ secao16-rotas-em-um-app-vuejs/258-tornando-router-links-mais-dinamico/src/router.js
@@ -0,0 +1,26 @@
+import Vue from "vue"
+import Router from "vue-router"
+import Home from "./components/Home"
+
+import User from "./components/user/User"
+import UserList from "./components/user/UserList"
+import UserEdit from "./components/user/UserEdit"
+import UserDetail from "./components/user/UserDetail"
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+    path: '/',
+    component: Home
+  }, {
+    path: '/user',
+    component: User,
+    props: true,
+    children: [
+      {path: '', component: UserList},
+      {path: ':id', component: UserDetail, props: true},
+      {path: ':id/edit', component: UserEdit, props: true}
+    ]
+  }]
+})
