a0842c67c57cc236094c99523ce7adf700c08bc5
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-05-20 10:39:25 -0300

    Added lesson 281

---
+++ secao17-vuex/281-mapeando-getters/src/store/store.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    produtos: []
+  },
+  getters: {
+    valorTotal(state){
+      return state.produtos.map(p => p.quantidade * p.preco).reduce((total, atual) => total + atual, 0)
+    }
+  }
+})
